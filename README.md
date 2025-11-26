310630361ed2b3af19383b9337043fa42ece66f2
Ilkka Eskola <ilkka.eskola@yahoo.com>
2025-11-26 22:57:07 +0200

    Project done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # javascript-trivia-bot
+
 This lab project is part of the FreeCodeCamp's Certified Full Stack Developer Curriculum.
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Ilkka";
+let botLocation = "Finland";
+let favoriteLanguage = "javaScript";
+
+console.log("My name is " + botName + " and I live in " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I really love " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
+codingFact = "I could study " + favoriteLanguage + " all day long.";
+
+console.log(codingFact);
+
+codingFact = "I better get back to study " + favoriteLanguage + ".";
+
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
